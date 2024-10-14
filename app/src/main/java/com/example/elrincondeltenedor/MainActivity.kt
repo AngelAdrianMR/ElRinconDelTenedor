0e7677f99ec6770f493320285a95717d580d04fe
Angel <angel_adrian30@hotmail.com>
2024-10-14 12:56:01 +0200

    He creado la interfaz de home01

--- app/src/main/java/com/example/elrincondeltenedor/MainActivity.kt
+++ app/src/main/java/com/example/elrincondeltenedor/MainActivity.kt
@@ -3,18 +3,12 @@ package com.example.elrincondeltenedor
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.collection_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
