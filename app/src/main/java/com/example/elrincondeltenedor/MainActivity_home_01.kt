126f16ef6d1966ddc6d7a0f3848367ae00e03ff4
Angel <angel_adrian30@hotmail.com>
2024-10-18 10:42:21 +0200

    Refactorización de los nombres del archivo

--- app/src/main/java/com/example/elrincondeltenedor/MainActivity_home_01.kt
+++ app/src/main/java/com/example/elrincondeltenedor/MainActivity_home_01.kt
@@ -3,10 +3,8 @@ package com.example.elrincondeltenedor
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity_home_01 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
