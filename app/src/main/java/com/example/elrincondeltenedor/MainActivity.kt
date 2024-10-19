bc7398b799fdf2cc40ca2589fc6ed9cfaac9b7e6
Angel <angel_adrian30@hotmail.com>
2024-10-19 11:35:49 +0200

    Toolbar añadido y comienzo de menu

--- app/src/main/java/com/example/elrincondeltenedor/MainActivity.kt
+++ app/src/main/java/com/example/elrincondeltenedor/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.elrincondeltenedor
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.elrincondeltenedor.databinding.ActivityMainBinding
@@ -19,5 +22,24 @@ class MainActivity : AppCompatActivity() {
         binding = ValoracionesRecyclerviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    protected fun setupToolbarWithMenu() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val menuImageView: ImageView = findViewById(R.id.fotoPerfil)
+        menuImageView.setOnClickListener {
+            showPopupMenu(menuImageView)
+        }
+    }
+
+    private fun showPopupMenu(view: ImageView) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.menuInflater.inflate(R.menu.menu_options, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+
+
+        popupMenu.show()
     }
 }
