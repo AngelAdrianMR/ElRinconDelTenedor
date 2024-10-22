afac6046c87d4a32b85243c536786aac4022b11a
USER <marox130905@gmail.com>
2024-10-22 18:04:50 +0200

    He añadido la pantalla de login

---
+++ app/src/main/java/com/example/elrincondeltenedor/loginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.elrincondeltenedor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.elrincondeltenedor.databinding.LoginScreenBinding
+import com.example.elrincondeltenedor.databinding.SettingScreenBinding
+
+class loginFragment : Fragment() {
+    private lateinit var _binding: LoginScreenBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LoginScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
